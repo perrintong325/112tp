14f56ad90b117ae3ead8bebde5ef5e0b4cb31d3b
perrintong325 <perrintong@icloud.com>
2022-04-08 18:00:49 -0400

    hand tracking v0, seems a bit slow tho and loses track when moving too quick, only testing, need to change to OOP

---
+++ handTracking.py
@@ -0,0 +1,22 @@
+import cv2 
+import mediapipe as mp
+
+vid = cv2.VideoCapture(1)
+drawing = mp.solutions.drawing_utils
+hands = mp.solutions.hands.Hands()
+
+while True:
+    status, image = vid.read()
+    image = cv2.flip(image,1)
+    results = hands.process(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+    if results.multi_hand_landmarks:
+        for landmark in results.multi_hand_landmarks:
+            for num,pos in enumerate(landmark.landmark):
+                y,x,colour = image.shape
+                cx = int(pos.x * x)
+                cy = int(pos.y*y)
+                cv2.circle(image, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+            drawing.draw_landmarks(image,landmark,mp.solutions.hands.HAND_CONNECTIONS)
+    cv2.imshow("image",image)
+    cv2.waitKey(1)
+
