31250007c07200cde8b042e89b7a5bb6a34affbf
perrintong325 <perrintong@icloud.com>
2022-04-09 00:23:50 -0400

    cs3 hand tracking v0.1, drawing circle seems to be what's causing it to lag the most, multihand processing also a bit laggy, switched to OOP, probably seperate the files as well

--- cs3handTracking.py
+++ cs3handTracking.py
@@ -3,9 +3,35 @@ from PIL import Image
 import cv2
 import mediapipe as mp
 
-drawing = mp.solutions.drawing_utils
-hands = mp.solutions.hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 vid = cv2.VideoCapture(1)
+
+class handDetector(object):
+    def __init__(self,maxHands,minDetectionConf,minTrackConf):
+        self.maxHands = maxHands
+        self.minDetectionConf = minDetectionConf
+        self.minTrackConf = minTrackConf
+
+    def drawDots(self,image):
+        if self.results.multi_hand_landmarks:
+            for landmark in self.results.multi_hand_landmarks:
+                for pos in landmark.landmark:
+                        y,x,colour = image.shape
+                        # !!!won't run without int!!! keep it
+                        cx = int(pos.x * x)
+                        cy = int(pos.y*y)
+                        # drawCircle(cx,cy,15,fill='green')
+                        cv2.circle(image, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+    
+    def findHands(self,image):
+        self.results = mp_hands.Hands(max_num_hands = self.maxHands,
+                                min_detection_confidence=self.minDetectionConf,
+                                min_tracking_confidence=self.minTrackConf).process(image)
+        if self.results.multi_hand_landmarks:
+            for landmark in self.results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(image,landmark,mp_hands.HAND_CONNECTIONS)
+        return image
 
 def onAppStart(app):
     app.vid = vid
@@ -14,26 +40,12 @@ def onAppStart(app):
 def redrawAll(app):
     if app.hasFrame:
         image = cv2.flip(cv2.cvtColor(app.frame, cv2.COLOR_BGR2RGB),1)
-        results = hands.process(image)
-        if results.multi_hand_landmarks:
-            for landmark in results.multi_hand_landmarks:
-                drawing.draw_landmarks(image,landmark,mp.solutions.hands.HAND_CONNECTIONS) 
-            drawPILImage(Image.fromarray(image), 0, 0)
-            for landmark in results.multi_hand_landmarks:
-                drawing.draw_landmarks(image,landmark,mp.solutions.hands.HAND_CONNECTIONS)
-                for num,pos in enumerate(landmark.landmark):
-                    y,x,colour = image.shape
-                    # !!!won't run without int!!! keep it
-                    cx = int(pos.x * x)
-                    cy = int(pos.y*y)
-                    # cv2.circle(rgbImage, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-                    drawCircle(cx,cy,15,fill='green')
-        else:
-            drawPILImage(Image.fromarray(image), 0, 0)
-                
+        image = app.handDetector.findHands(image)
+        app.handDetector.drawDots(image)
+        drawPILImage(Image.fromarray(image), 0, 0)
         
-
 def onStep(app):
+    app.handDetector = handDetector(4,0.5,0.5)
     status, app.frame = app.vid.read()
     app.hasFrame = True
 
@@ -44,23 +56,3 @@ def main():
     
 
 main()
-
-
-
-# #reference: google mediaPipe docs
-# while True:
-#     status, image = vid.read()
-#     image = cv2.flip(image,1)
-#     results = hands.process(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
-#     if results.multi_hand_landmarks:
-#         for landmark in results.multi_hand_landmarks:
-#             for num,pos in enumerate(landmark.landmark):
-#                 y,x,colour = image.shape
-#                 # !!!won't run without int!!! keep it
-#                 cx = int(pos.x * x)
-#                 cy = int(pos.y*y)
-#                 cv2.circle(image, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-#             drawing.draw_landmarks(image,landmark,mp.solutions.hands.HAND_CONNECTIONS)
-#     cv2.imshow("image",image)
-#     cv2.waitKey(1)
-
