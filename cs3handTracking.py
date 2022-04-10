3b7cbd4ad2cca4f954f6c84187ed0aec2426b5f3
perrintong325 <perrintong@icloud.com>
2022-04-10 16:55:35 -0400

    tech demo v1, head tracking added, increasing SpS doesn't seem to help with fram rate probably because of PIL

--- cs3handTracking.py
+++ cs3handTracking.py
@@ -7,6 +7,7 @@ mp_draw = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 vid = cv2.VideoCapture(1)
 
+#reference: Google MediaPipe doc
 class handDetector(object):
     def __init__(self,maxHands,minDetectionConf,minTrackConf):
         self.maxHands = maxHands
@@ -19,33 +20,62 @@ class handDetector(object):
                 for pos in landmark.landmark:
                         y,x,colour = image.shape
                         # !!!won't run without int!!! keep it
-                        cx = int(pos.x * x)
-                        cy = int(pos.y*y)
-                        # drawCircle(cx,cy,15,fill='green')
-                        cv2.circle(image, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+                        cx = pos.x * x
+                        cy = pos.y*y
+                        drawCircle(cx,cy,15,fill='green')
+                        #cv2.circle(image, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
     
     def findHands(self,image):
         self.results = mp_hands.Hands(max_num_hands = self.maxHands,
-                                min_detection_confidence=self.minDetectionConf,
-                                min_tracking_confidence=self.minTrackConf).process(image)
+                    min_detection_confidence=self.minDetectionConf,
+                    min_tracking_confidence=self.minTrackConf).process(image)
         if self.results.multi_hand_landmarks:
             for landmark in self.results.multi_hand_landmarks:
                 mp_draw.draw_landmarks(image,landmark,mp_hands.HAND_CONNECTIONS)
         return image
 
+mp_pose = mp.solutions.pose
+mp_face_detection = mp.solutions.face_detection
+
+class headDetector(object):
+    def __init__(self,minDetectionConf):
+        self.minDetectionConf = minDetectionConf
+    
+    def findHeads(self,image):
+        self.results = mp_face_detection.FaceDetection(min_detection_confidence=self.minDetectionConf).process(image)
+        if self.results.detections:
+            for head in self.results.detections:
+                mp_draw.draw_detection(image,head)
+        return image
+
 def onAppStart(app):
     app.vid = vid
     app.hasFrame = False
+    app.handMode = 1
+    app.stepsPerSecond = 600
 
 def redrawAll(app):
     if app.hasFrame:
         image = cv2.flip(cv2.cvtColor(app.frame, cv2.COLOR_BGR2RGB),1)
-        image = app.handDetector.findHands(image)
-        app.handDetector.drawDots(image)
-        drawPILImage(Image.fromarray(image), 0, 0)
-        
+        if app.handMode == 1:
+            image = app.handDetector.findHands(image)
+            drawPILImage(Image.fromarray(image), 0, 0)
+            # cv2.imshow("image",image)
+            app.handDetector.drawDots(image)
+        else:
+            image = app.headDetector.findHeads(image)
+            drawPILImage(Image.fromarray(image), 0, 0)
+    drawLabel(f"Hand Mode = {bool(app.handMode)}",100,100,size = 30,
+                fill='lawnGreen', align='left')
+
+def onKeyPress(app,key):
+    if key == 'space':
+        app.handMode -= 1
+        app.handMode = abs(app.handMode)
+
 def onStep(app):
-    app.handDetector = handDetector(4,0.5,0.5)
+    app.handDetector = handDetector(4,0.3,0.3)
+    app.headDetector = headDetector(0.3)
     status, app.frame = app.vid.read()
     app.hasFrame = True
 
@@ -53,6 +83,5 @@ def main():
     _,image = vid.read()
     y,x,c = image.shape
     runApp(width=x,height=y)
-    
 
 main()
